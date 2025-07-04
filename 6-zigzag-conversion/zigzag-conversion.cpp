8aea2c2ed4c8f899b76ed35da7c1c6818f62a864
AnshikaTrivedi1 <anshikatrivedi114@gmail.com>
2025-07-04 23:30:44 +0530

    Time: 3 ms (76.74%) | Memory: 14.1 MB (55.27%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        
+        vector<string> rows(numRows);
+        int currentRow = 0;
+        bool goingDown = false;
+        
+        for (char c : s) {
+            rows[currentRow] += c;
+            
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            
+            currentRow += goingDown ? 1 : -1;
+        }
+        
+        string result;
+        for (string row : rows) {
+            result += row;
+        }
+        
+        return result;
+    }
+};
+
+class SolutionMath {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        
+        string result;
+        int n = s.length();
+        int cycleLen = 2 * numRows ;
+        
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                result += s[j + i];
+                
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+                    result += s[j + cycleLen - i];
+                }
+            }
+        }
+        
+        return result;
+    }
+};
